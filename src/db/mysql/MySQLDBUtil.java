b53ffeffc0e33599f2e8278e6a5d45008c6c49bc
Spencer <zqian0336@gmail.com>
2020-04-09 17:09:29 -0700

    deployed

--- src/db/mysql/MySQLDBUtil.java
+++ src/db/mysql/MySQLDBUtil.java
@@ -1,11 +1,11 @@
 package db.mysql;
 
 public class MySQLDBUtil {
-    private static final String HOSTNAME = "localhost"; //localhost
+    private static final String HOSTNAME = "eventbox-mysql.cu2gba3klpnn.us-west-1.rds.amazonaws.com"; //localhost
     private static final String PORT_NUM = "3306"; // change it to your mysql port number[8889]
-    public static final String DB_NAME = "eventboxdb";
+    public static final String DB_NAME = "eventbox-db"; //eventboxdb
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Qian2012";
+    private static final String PASSWORD = "root";
     public static final String URL = "jdbc:mysql://"
             + HOSTNAME + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
